849e362943a891031f424c7e51aae0f39d240ef6
kiksun <kimera4173@gmail.com>
2024-07-15 20:34:33 +0900

    delete initial data

--- problem1/mysql/0_init.sql
+++ problem1/mysql/0_init.sql
@@ -18,33 +18,3 @@ CREATE TABLE `block_list` (
   `user2_id` int(11) NOT NULL,
   PRIMARY KEY (`id`)
 );
-
-INSERT INTO users (user_id, name) VALUES (1,"A");
-INSERT INTO users (user_id, name) VALUES (2,"B");
-INSERT INTO users (user_id, name) VALUES (3,"C");
-INSERT INTO users (user_id, name) VALUES (4,"D");
-INSERT INTO users (user_id, name) VALUES (5,"E");
-INSERT INTO users (user_id, name) VALUES (6,"F");
-INSERT INTO users (user_id, name) VALUES (7,"G");
-INSERT INTO users (user_id, name) VALUES (8,"H");
-INSERT INTO users (user_id, name) VALUES (9,"I");
-INSERT INTO users (user_id, name) VALUES (10,"J");
-
-INSERT INTO friend_link (user1_id, user2_id) VALUES (1,2);
-INSERT INTO friend_link (user1_id, user2_id) VALUES (1,3);
-INSERT INTO friend_link (user1_id, user2_id) VALUES (1,4);
-INSERT INTO friend_link (user1_id, user2_id) VALUES (1,5);
-INSERT INTO friend_link (user1_id, user2_id) VALUES (1,7);
-INSERT INTO friend_link (user1_id, user2_id) VALUES (1,8);
-INSERT INTO friend_link (user1_id, user2_id) VALUES (1,9);
-INSERT INTO friend_link (user1_id, user2_id) VALUES (1,10);
-INSERT INTO friend_link (user1_id, user2_id) VALUES (2,3);
-INSERT INTO friend_link (user1_id, user2_id) VALUES (2,6);
-INSERT INTO friend_link (user1_id, user2_id) VALUES (2,7);
-INSERT INTO friend_link (user1_id, user2_id) VALUES (2,8);
-INSERT INTO friend_link (user1_id, user2_id) VALUES (3,6);
-INSERT INTO friend_link (user1_id, user2_id) VALUES (3,9);
-INSERT INTO friend_link (user1_id, user2_id) VALUES (3,10);
-;
-INSERT INTO block_list (user1_id, user2_id) VALUES (1,10);
-INSERT INTO block_list (user1_id, user2_id) VALUES (3,6);
